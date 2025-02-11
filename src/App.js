ae98202454f27f855bb722076a6766f63a3c5f65
Huyen Trang <tranglee150604@gmail.com>
2025-02-11 23:02:51 +0700

    Bai moi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import style from './App.module.sass';
+import { clsx } from 'clsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          huhuuruufhh  eee
-        </a>
-      </header>
+    <div>
+      <h1 className={clsx(style.title, style.content)}>
+        Le Huyen Trang 
+      </h1>
+      <p>Cau oi</p>
     </div>
-  );
+  )
 }
-
 export default App;
